406ca8a07eb9fb1ee435cfd7934f303d71a40354
likanozadze <145837358+likanozadze@users.noreply.github.com>
2024-07-12 15:54:11 +0400

    initial commit

---
+++ MovieBookingApplication/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieBookingApplication
+//
+//  Created by Lika Nozadze on 7/12/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 26/255.0, green: 34/255.0, blue: 50/255.0, alpha: 1)
+    }
+
+
+}
+
