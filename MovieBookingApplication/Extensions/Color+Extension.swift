981569de1a52843ff52e3a2f3dd058580793fa3f
likanozadze <145837358+likanozadze@users.noreply.github.com>
2024-07-12 20:22:57 +0400

    update colors

--- MovieBookingApplication/Extensions/Color+Extension.swift
+++ MovieBookingApplication/Extensions/Color+Extension.swift
@@ -10,9 +10,8 @@ import SwiftUI
 
 extension UIColor {
     static let customAccentColor = UIColor(red: 255/255, green: 87/255, blue: 0/255, alpha: 1)
-    static let customBackgroundColor = UIColor(red: 26/255.0, green: 34/255.0, blue: 50/255.0, alpha: 1)
+    static let customBackgroundColor = UIColor(red: 30/255.0, green: 30/255.0, blue: 30/255.0, alpha: 1)
     static let customSecondaryColor = UIColor(red: 33/255, green: 33/255, blue: 36/255, alpha: 1)
-    static let customGreenColor = UIColor(red: 184/255, green: 255/255, blue: 124/255, alpha: 1)
 }
 
 extension Color {
